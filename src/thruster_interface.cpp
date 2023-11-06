f8b362bcb6da48802da516275d09606493a210d1
r <srinidhip1234@gmail.com>
2023-11-06 18:36:03 +0530

    fncs updated_1

--- src/thruster_interface.cpp
+++ src/thruster_interface.cpp
@@ -1,7 +1,25 @@
+
 #include "thruster_interface.hpp"
+#include Servo.h
+#define NO_OF_THRUSTERS=7;
 
-inline void initializeThrusters() {
+  //declaring an array of servo objects for the thrusters
+Servo thrusters[NO_OF_THRUSTERS];
+  //pinmap for the thrusters
+const uint8_t pinMap[NO_OF_THRUSTERS] = {PA0, PA1, PA2, PA3, PA6, PB0, PA7};
+
+inline void initializeThrusters(){
+  // Initializing the thrusters
+  for (int i = 0; i < NO_OF_THRUSTERS; i++){
+    thrusters[i].attach(pinMap[i]);
+    thrusters[i].writeMicroseconds(1472);
+  }
+}
+ //set speed for each thruster based on the vector input
+inline void setThrusterSpeeds(std::vector<int16_t> speeds){
+  for (int i = 0; i < NO_OF_THRUSTERS; i++){
+    int pwm_value = speeds[i];
+    thrusters[i].writeMicroseconds(pwm_value);
+  }
 }
 
-inline void setThrusterSpeeds(std::vector<int16_t> speeds) {
-}
