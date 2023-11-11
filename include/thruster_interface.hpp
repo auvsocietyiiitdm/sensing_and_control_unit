4152638fdf1a458a5e65ce5420f123adb84c87fb
nidhi <srinidhip1234@gmail.com>
2023-11-11 19:11:56 +0530

    Remove sensing_and_control_unit directory

--- include/thruster_interface.hpp
+++ include/thruster_interface.hpp
@@ -1,6 +1,7 @@
 #ifndef THRUSTER_INTERFACE_HPP
 #define THRUSTER_INTERFACE_HPP
 #include <Arduino.h>
-inline void initializeThrusters();
-inline void setThrusterSpeeds(std::vector<int16_t> speeds);
+#include <Servo.h> 
+inline void initializeThrusters()
+inline void setThrusterPWMValues(const int16_t pwm_values[])
 #endif // THRUSTER_INTERFACE_HPP
--- src/thruster_interface.cpp
+++ src/thruster_interface.cpp
@@ -1,23 +1,23 @@
 #include "thruster_interface.hpp"
-#include "config.h"
+#include "config.hpp"
 
-Servo g_thrusters[NO_OF_THRUSTERS];
+Servo g_thrusters[NUMBER_OF_THRUSTERS];
   //pinmap for the thrusters
-const uint8_t pinMap[NO_OF_THRUSTERS] = {PA0, PA1, PA2, PA3, PA6, PB0, PA7};
+const uint8_t pinMap[NUMBER_OF_THRUSTERS] = {PA0, PA1, PA2, PA3, PA6, PB0, PA7};
 
 inline void initializeThrusters(){
   // Initializing the thrusters
-  for (int thruster_index = 0; i < thruster_index; thruster_index++){
-    g_thrusters[i].attach(pinMap[i]);
-    g_thrusters[i].writeMicroseconds(INIT_THRUSTER_PWM);
+  for (int thruster_index = 0; thruster_index < NUMBER_OF_THRUSTERS; thruster_index++){
+    g_thrusters[thruster_index].attach(pinMap[thruster_index]);
+    g_thrusters[thruster_index].writeMicroseconds(INIT_THRUSTER_PWM);
   }
 }
 
 inline void setThrusterPWMValues(const int16_t pwm_values[]){
   int pwm_value;
-  for (int thruster_index = 0; i < thruster_index; thruster_index++){
-    pwm_value = speeds[i];
-    g_thrusters[i].writeMicroseconds(pwm_value);
+  for (int thruster_index = 0; thruster_index < NUMBER_OF_THRUSTERS; thruster_index++){
+    pwm_value = pwm_values[thruster_index];
+    g_thrusters[thruster_index].writeMicroseconds(pwm_value);
   }
 }
 
